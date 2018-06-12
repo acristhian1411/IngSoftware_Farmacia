7357268a58fe681bb65f4067a5fb37ff233ca476
usuario <ljbarbozagauto77@gmail.com>
2018-06-11 23:03:43 -0400

    farmacia

--- farmacia/app/models/ability.rb
+++ farmacia/app/models/ability.rb
@@ -1,15 +1,23 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-       user ||= User.new # guest user (not logged in)
-       if user.admin?
-         can :manage, :all, :destroy, :update
-       else
-         can :read, :all, 
-       end
+   user ||= User.new
+
+        if User.current_role == 'admin' 
+            can :manage, :all 
+        else
+           
+                can :read, Producto
+                can :update, Producto
+                cannot :destroy, Products
+                cannot :create, Products
+
+                cannot :manage, Client
+            
+
+            can :read, :all
+        end
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
